1e25f0234ebf93912d49ff59d31f0ce9e883b943
Ben Fitzgerald <theeuclidean@gmail.com>
2014-08-18 16:24:05 +0100

    Going back to conditional & corrected init

--- lib/spree/wombat/client_base.rb
+++ lib/spree/wombat/client_base.rb
@@ -5,7 +5,7 @@ module Spree
   module Wombat
     class ClientBase
 
-      def initialize
+      def initialize(object)
         @last_push_time = Spree::Wombat::Config[:last_pushed_timestamps][object] || Time.now
         @this_push_time = touch_last_pushed(object)
         @payload_builder = Spree::Wombat::Config[:payload_builder][object]
@@ -13,10 +13,13 @@ module Spree
 
       def self.get_items(object, datetime_column: 'updated_at', batch_size: 10)
         scope = object.constantize
-        unless filter != @payload_builder[:filter]
-          raise Exception
+        # unless filter != @payload_builder[:filter]
+        #   raise Exception
+        # end
+        # scope.send(filter.to_sym)
+        if filter = payload_builder[:filter]
+          scope = scope.send(filter.to_sym)
         end
-        scope.send(filter.to_sym)
         items = []
         scope.where(datetime_column.to_sym => @last_push_time..@this_push_time).find_in_batches(batch_size: batch_size) do |batch|
           items << serilize(batch)
